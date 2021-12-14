8cdd3bf8781272d6ff851f7d5226f603f0843dc6
Camila <camilahhhaquehua@gmail.com>
2021-12-14 12:53:33 -0500

    uwu

---
+++ js/script.js
@@ -0,0 +1,55 @@
+let A = document.getElementById("A");
+let play = document.getElementById("play");
+play.addEventListener("click", () =>{
+A.play();
+})
+
+let S = document.getElementById("S");
+let play1 = document.getElementById("play1");
+play1.addEventListener("click", () =>{
+  S.play();
+})
+
+let D = document.getElementById("D");
+let play2 = document.getElementById("play2");
+play2.addEventListener("click", () =>{
+  D.play();
+})
+
+let F = document.getElementById("F");
+let play3 = document.getElementById("play3");
+play3.addEventListener("click", () =>{
+  F.play();
+})
+
+let G = document.getElementById("G");
+let play4 = document.getElementById("play4");
+play4.addEventListener("click", () =>{
+  G.play();
+})
+
+let H = document.getElementById("H");
+let play5 = document.getElementById("play5");
+play5.addEventListener("click", () =>{
+  H.play();
+})
+
+let J = document.getElementById("J");
+let play6 = document.getElementById("play6");
+play6.addEventListener("click", () =>{
+  J.play();
+})
+
+let K= document.getElementById("K");
+let play7 = document.getElementById("play7");
+play7.addEventListener("click", () =>{
+K.play();
+})
+
+let L = document.getElementById("L");
+let pay8 = document.getElementById("play8");
+play8.addEventListener("click", () =>{
+  L.play();
+})
+
+
